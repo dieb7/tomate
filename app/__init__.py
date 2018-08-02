5a332098c9960340d185d752e7133ce0410a42ed
Diego Benavides <diegob41@gmail.com>
2018-08-02 11:11:56 -0600

    Reorganiced code to use flask blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp)
+
+    return app
 
 
 # to avoid circular dependency issue with flask imports
-from app import routes, models
+from app import models
---
+++ app/api/routes.py
@@ -0,0 +1,7 @@
+from app.api import bp
+
+
+@bp.route('/')
+@bp.route('/index')
+def index():
+    return "Hello, World!"
